f3035822f7e6891952d54d99af9446cb9bcf19ed
hadesxkore <90496922+hadesxkore@users.noreply.github.com>
2024-04-21 21:33:01 +0800

    Update SignUpPage.js

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,40 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
 import googleIcon from '../images/google.png';
-import backIcon from '../images/back.png'; // Import the back icon image
+import backIcon from '../images/back.png';
 import { auth, GoogleAuthProvider } from '../firebase';
 import './LoginPage.css';
+
 function SignUpPage() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [loading, setLoading] = useState(false); // State for loading animation
+  const [loading, setLoading] = useState(false);
+  const [passwordStrength, setPasswordStrength] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        if (!user.emailVerified) {
+          setErrorMessage('Please verify your email before logging in.');
+          auth.signOut(); // Sign out the user if email is not verified
+        } else {
+          navigate('/');
+        }
+      }
+    });
+    return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const signUpWithEmailPassword = async (email, password) => {
     try {
-      // Check if password and confirm password match
       if (password !== confirmPassword) {
-        setErrorMessage("Passwords do not match.");
+        setErrorMessage('Passwords do not match.');
         return;
       }
 
-      setLoading(true); // Start loading animation
-
-      // Create user with email and password
+      setLoading(true);
       const userCredential = await auth.createUserWithEmailAndPassword(email, password);
-      // Send email verification
       await userCredential.user.sendEmailVerification();
-      // Redirect to VerifyPage and pass email as a parameter
       navigate(`/verify?email=${email}`);
     } catch (error) {
       console.error(error.message);
       setErrorMessage(error.message);
     } finally {
-      setLoading(false); // Stop loading animation
+      setLoading(false);
     }
   };
 
@@ -51,25 +63,63 @@ function SignUpPage() {
     e.preventDefault();
     signUpWithEmailPassword(email, password);
   };
+
+  const calculatePasswordStrength = (password) => {
+    const specialCharRegex = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+    const numberRegex = /[0-9]/;
+    const uppercaseRegex = /[A-Z]/;
+
+    const hasSpecialChar = specialCharRegex.test(password);
+    const hasNumber = numberRegex.test(password);
+    const hasUppercase = uppercaseRegex.test(password);
+
+    if (password.length >= 8 && hasSpecialChar && hasNumber && hasUppercase) {
+      setPasswordStrength('Strong');
+    } else if (password.length >= 6 && (hasSpecialChar || hasNumber || hasUppercase)) {
+      setPasswordStrength('Moderate');
+    } else {
+      setPasswordStrength('Weak');
+    }
+  };
+
+  const isPasswordValid = () => {
+    const specialCharRegex = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+    const numberRegex = /[0-9]/;
+    const uppercaseRegex = /[A-Z]/;
+
+    const hasSpecialChar = specialCharRegex.test(password);
+    const hasNumber = numberRegex.test(password);
+    const hasUppercase = uppercaseRegex.test(password);
+
+    return password.length >= 8 && hasSpecialChar && hasNumber && hasUppercase;
+  };
+
+  const showErrorMessage = () => {
+    if (password && !isPasswordValid()) {
+      return 'Please follow the password instructions.';
+    }
+    return errorMessage;
+  };
+
   return (
-    <div className="signup-container" style={{ backgroundColor: '#eeeeee' }}>
+    <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundColor: '#eeeeee' }}>
       <div className="logo-container text-center">
         <img src={logo} alt="Logo" className="logo" style={{ width: '200px', margin: '0 auto'}} />
       </div>
       <button 
-  onClick={() => navigate('/')} 
-  className="back-button">
-  <img src={backIcon} alt="Back" /> {/* Add margin-right for spacing */}
-  <span className="button-text font-semibold">Homepage</span>
-</button>
+        onClick={() => navigate('/')} 
+        className="back-button"
+      >
+        <img src={backIcon} alt="Back" />
+        <span className="button-text font-semibold">Homepage</span>
+      </button>
 
-
-      <div className="signup-form-container" style={{ maxWidth: '500px', margin: '0 auto' }}>
-        <form id="signup-form" className="bg-white p-8 rounded-lg shadow-md mx-auto" onSubmit={handleSignUp}>
-          <h2 className="text-4xl font-bold mb-6 text-LEFT">Sign Up</h2>
-          {errorMessage && <p className="text-red-600 mb-4">{errorMessage}</p>}
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
+      <div className="signup-form-container" style={{ maxWidth: '450px', marginTop: '-1rem' }}>
+        <form id="signup-form" className="bg-white p-4 sm:p-8 rounded-lg shadow-md mx-auto" onSubmit={handleSignUp}>
+          <h2 className="text-3xl sm:text-4xl font-bold mb-4 sm:mb-6 text-LEFT">Sign Up</h2>
+          {showErrorMessage() && <p className="text-red-600 mb-2 sm:mb-4">{showErrorMessage()}</p>}
+          <div className="mb-2 sm:mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-1 sm:mb-2" htmlFor="email">Email</label>
             <input 
               className="rounded-lg shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-custom-input" 
               id="email" 
@@ -79,21 +129,32 @@ function SignUpPage() {
               onChange={(e) => setEmail(e.target.value)} 
             />
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+          <div className="mb-1 sm:mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-1 sm:mb-2" htmlFor="password">Password</label>
             <input 
-              className="rounded-lg shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-custom-input" 
+              className="rounded-lg shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 sm:mb-3 leading-tight focus:outline-none focus:shadow-outline bg-custom-input" 
               id="password" 
               type="password" 
               placeholder="Password" 
               value={password}
-              onChange={(e) => setPassword(e.target.value)} 
+              onChange={(e) => {
+                setPassword(e.target.value);
+                calculatePasswordStrength(e.target.value);
+              }} 
             />
+            {password && (
+              <p className={`text-xs mb-1 ${passwordStrength === 'Weak' ? 'text-red-500' : passwordStrength === 'Moderate' ? 'text-orange-500' : 'text-green-500'}`}>
+                Password Strength: {passwordStrength}
+              </p>
+            )}
+            <p className="text-xs mb-1 text-gray-600">
+              Password must contain at least 8 characters, including one special character, one number, and one uppercase letter.
+            </p>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirmPassword">Confirm Password</label>
+          <div className="mb-1 sm:mb-2">
+            <label className="block text-gray-700 text-sm font-bold mb-1 sm:mb-2" htmlFor="confirmPassword">Confirm Password</label>
             <input 
-              className="rounded-lg shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-custom-input" 
+              className="rounded-lg shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-1 sm:mb-3 leading-tight focus:outline-none focus:shadow-outline bg-custom-input" 
               id="confirmPassword" 
               type="password" 
               placeholder="Confirm Password" 
@@ -101,34 +162,34 @@ function SignUpPage() {
               onChange={(e) => setConfirmPassword(e.target.value)} 
             />
           </div>
-          <button className="bg-black text-white font-bold py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline hover:bg-gray-900 mt-3 w-full" type="submit">
+          <button 
+            className="bg-black text-white font-bold py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline hover:bg-gray-900 mt-1 w-full" 
+            type="submit"
+            disabled={!isPasswordValid()} 
+          >
             {loading ? 'Creating Account...' : 'Create Account'}
           </button>
-          <div className="flex justify-center items-center"> 
-            <span className="text-gray-900 text-bigger">or</span>
+          <div className="flex justify-center items-center mt-1"> 
+            <span className="text-gray-900 text-sm sm:text-bigger">or</span>
           </div>
           <div className="flex justify-center items-center mb-2"> 
-            <button className="bg-white border border-gray-300 text-gray-700 font-bold py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline hover:bg-gray-200 mt-3 w-full" type="button" onClick={signInWithGoogle}>
+            <button className="bg-white border border-gray-300 text-gray-700 font-bold py-2 px-3 rounded-lg focus:outline-none focus:shadow-outline hover:bg-gray-200 mt-1 w-full" type="button" onClick={signInWithGoogle}>
               <img src={googleIcon} alt="Google Icon" className="inline-block w-4 h-4 mr-2" />
               Sign Up with Google
             </button>
           </div>
-          <p className="text-xs text-gray-700 mb-4">
+          <p className="text-xs text-gray-700 mb-1 sm:mb-4">
             By clicking "Create account" above, you acknowledge that you will receive updates from the WhereHouse team and that you have read, understood, and agreed to WhereHouse Library's Terms & Conditions, Licensing Agreement and Privacy Policy.
           </p>
           <hr className="line" />
-          <div className="cta-container flex justify-between items-center mb-2 pt-4">
-            <span className="text-gray-900 text-bigger">Already have an account?</span>
+          <div className="cta-container flex justify-between items-center mb-1 sm:mb-2 pt-2 sm:pt-4">
+            <span className="text-gray-900 text-sm sm:text-bigger">Already have an account?</span>
             <Link to="/login" className="bg-transparent border-2 border-custom-color text-custom-color font-relative-pro py-1.5 px-4 rounded-lg focus:outline-none focus:shadow-outline login-button hover:bg-gray-200 hover:text-gray-900">Login</Link>
           </div>
         </form>
       </div>
     </div>
   );
-
-
-
-
 }
 
 export default SignUpPage;
